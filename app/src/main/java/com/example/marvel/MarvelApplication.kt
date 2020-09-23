9fe174257d177db308d22f39c0b56736a3252733
Alex <agarcia992@gmail.com>
2020-09-23 13:29:27 +0200

    Adding main activity and application

--- app/src/main/java/com/example/marvel/MarvelApplication.kt
+++ app/src/main/java/com/example/marvel/MarvelApplication.kt
@@ -1,4 +1,4 @@
-package com.example.marvel.di
+package com.example.marvel
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
---
+++ app/src/main/java/com/example/marvel/presentation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.marvel.presentation
+
+import android.app.Activity
+
+class MainActivity : Activity() {
+}
